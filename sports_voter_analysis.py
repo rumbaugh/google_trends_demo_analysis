575db82cc1787dc19e1f85f6c361feba0ae643a4
Nick Rumbaugh <takkyon13@gmail.com>
2017-11-03 16:55:27 -0700

    Added classification of regions based on sports web traffic

--- sports_voter_analysis.py
+++ sports_voter_analysis.py
@@ -1,18 +1,24 @@
 import numpy as np
 import pandas as pd
 
+from sklearn import decomposition,neighbors
+from sklearn.neighbors import KDTree,BallTree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from plotting import *
 
 class Demo:
     def __init__(self, masterfile='NFL_fandom_data-google_trends.csv', basepath='./', region_dict=None):
         self.df = pd.read_csv(basepath + masterfile , skiprows=1)
         if region_dict == None:
-            region_dict = {'South': np.array(['AL','AR','FL','GA','KY','LA','MS','NC','SC','OK','TN','TX','VA']), 'West': np.array(['AZ','CA','CO','ID','MT','NV','NM','OR','UT','WA','WY']), 'East': np.array(['CT','DE','DC','ME','MD','MA','NH','NJ','NY','PA','RI','VT']), 'Midwest': np.array(['IL','IN','IA','KS','MI','MO','NE','ND','SD','OH','WI','MN'])} 
+            region_dict = {'South': np.array(['AL','AR','FL','GA','KY','LA','MS','NC','SC','OK','TN','TX','VA']), 'West': np.array(['AZ','CA','CO','ID','MT','NV','NM','OR','UT','WA','WY']), 'East': np.array(['CT','DE','DC','ME','MD','MA','NH','NJ','NY','PA','RI','VT','WV']), 'Midwest': np.array(['IL','IN','IA','KS','MI','MO','NE','ND','SD','OH','WI','MN'])} 
         self.region_dict = region_dict
         self.state_to_region_dict = {}
         for reg, states in region_dict.items():
             for state in states: self.state_to_region_dict[state] = reg
         self.set_states()
         self.set_regions()
+        self.NCV=5
         
     def parse_state(self, instring):
         strarr = np.array(list(instring))
@@ -30,8 +36,8 @@ class Demo:
 
     def set_states(self):
         locations = self.df.DMA.values
-        self.df['state'] = np.zeros(len(df), dtype='|S8')
-        for i in range(0, len(df)):
+        self.df['state'] = np.zeros(len(self.df), dtype='|S8')
+        for i in range(0, len(self.df)):
             self.df.state.iloc[i] = ','.join(self.parse_state(locations[i]))
 
     def set_regions(self):
@@ -40,8 +46,8 @@ class Demo:
         except AttributeError:
             print 'Must set states'
             return
-        self.df['region'] = np.zeros(len(df), dtype='|S24')
-        for i in range(0, len(df)): 
+        self.df['region'] = np.zeros(len(self.df), dtype='|S24')
+        for i in range(0, len(self.df)): 
             cur_states = self.df.state.iloc[i].split(',')
             try:
                 self.df.region.iloc[i] = self.state_to_region_dict[cur_states[0]]
@@ -55,11 +61,49 @@ class Demo:
                 if not(curreg in self.df.region.iloc[i].split(',')):
                     self.df.region.iloc[i] = '{},{}'.format(self.df.region.iloc[i],curreg)
 
-
-                
-                
-
-
+    def reg_classify(self,technique = 'RF', usevotes = False, remove_regs_other = True, remove_mult_regs = True, sports = ['NFL', 'NBA', 'MLB', 'NHL', 'NASCAR', 'CBB', 'CFB'],train_perc=0.9, NE=10, NN=5, algorithm = 'kd_tree'):
+        #Attempts to classify regions based on the percentage
+        #of web traffic for different sports leagues, plus
+        #optionally Trump 2016 Vote % as well
+        if ((usevotes) & (not('Trump 2016 Vote' in sports))): sports = np.append(sports, 'Trump 2016 Vote')
+        X = self.df[sports]
+        regions = self.df.region
+        if remove_regs_other:
+            X, regions = X[regions != 'Other'], regions[regions != 'Other']
+        regions_list = regions.unique()
+        if remove_mult_regs:
+            for reg in regions_list:
+                if ',' in reg:
+                    X, regions, regions_list = X[regions != reg], regions[regions != reg], regions_list[regions_list != reg]
+        if technique == 'RF':
+            self.RFClassify(X,regions,train_perc,NE)
+        elif technique == 'NN':
+            self.NNClassify(X,regions,train_perc,NN,algorithm)
+        else:
+            print "Invalid technique chosen. Current options are: RF, NN."
+            return
 
     
-        
+    def RFClassify(self,X,target,train_perc=0.9,NE=10):
+        #Perform random forest classification 
+        #Uses a fraction of the sports array as the training set, defined
+        #by train_perc. The rest is used as the test set
+        train_df = X[:int(train_perc*np.shape(target)[0])]
+        train_X,train_y=train_df, target[:int(train_perc*len(target))]
+        test_X,test_y=X[int(train_perc*np.shape(X)[0]):],target[int(train_perc*np.shape(X)[0]):]
+        self.clf=RandomForestClassifier(n_estimators=NE)
+        self.clf.fit(train_X,train_y)
+        self.predicted_regions=self.clf.predict(test_X)
+        self.CV_scores=cross_val_score(self.clf,X,target,cv=self.NCV)
+    
+    def NNClassify(self,X,target,train_perc=0.9,n_neighbors=5, algorithm='kd_tree',weights='distance'):
+        #Perform nearest neighbor classification
+        #Uses a fraction of the sports array as the training set, defined
+        #by train_perc. The rest is used as the test set
+        train_df = X[:int(train_perc*np.shape(target)[0])]
+        train_X,train_y=train_df, target[:int(train_perc*len(target))]
+        test_X,test_y=X[int(train_perc*np.shape(X)[0]):],target[int(train_perc*np.shape(X)[0]):]
+        self.clf=neighbors.KNeighborsClassifier(n_neighbors,weights=weights,algorithm=algorithm)
+        self.clf.fit(train_X,train_y)
+        self.predicted_y=self.clf.predict(test_X)
+        self.CV_scores=cross_val_score(self.clf,X,target,cv=self.NCV)
